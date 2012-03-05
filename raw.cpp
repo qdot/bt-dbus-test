0cda7943d1ee9f86d10ec08b3e614245f8555d5f
Kyle <kyle@nonpolynomial.com>
2012-03-04 20:02:53 -0800

    Indentation fixes

--- raw.cpp
+++ raw.cpp
@@ -53,7 +53,7 @@ public:
 
   int32_t dbus_returns_int32(DBusMessage *reply);
   uint32_t dbus_returns_uint32(DBusMessage *reply);
-	// std::string dbus_returns_string(DBusMessage *reply);
+  // std::string dbus_returns_string(DBusMessage *reply);
   bool dbus_returns_boolean(DBusMessage *reply);
 
   template<typename T>
@@ -63,58 +63,58 @@ public:
     int len = 0, prop_type = DBUS_TYPE_INVALID, prop_index = -1, type;
 
     if(dbus_message_iter_get_arg_type(&iter) != DBUS_TYPE_ARRAY) {
-			printf("This isn't a dictionary!\n");
-			return false;
-		}
+      printf("This isn't a dictionary!\n");
+      return false;
+    }
     dbus_message_iter_recurse(&iter, &dict);
     do {
-			len = 0;
-			if (dbus_message_iter_get_arg_type(&dict) != DBUS_TYPE_DICT_ENTRY)
-			{
-				printf("Not a dict entry!\n");
-				return false;
-			}            
-			dbus_message_iter_recurse(&dict, &dict_entry);
-			DBusMessageIter prop_val, array_val_iter;
-			char *property = NULL;
-			uint32_t array_type;
-			char *str_val;
-			int i, j, type, int_val;
+      len = 0;
+      if (dbus_message_iter_get_arg_type(&dict) != DBUS_TYPE_DICT_ENTRY)
+      {
+        printf("Not a dict entry!\n");
+        return false;
+      }            
+      dbus_message_iter_recurse(&dict, &dict_entry);
+      DBusMessageIter prop_val, array_val_iter;
+      char *property = NULL;
+      uint32_t array_type;
+      char *str_val;
+      int i, j, type, int_val;
 
-			if (dbus_message_iter_get_arg_type(&dict_entry) != DBUS_TYPE_STRING) {
-				printf("Iter not a string!\n");
+      if (dbus_message_iter_get_arg_type(&dict_entry) != DBUS_TYPE_STRING) {
+        printf("Iter not a string!\n");
         return false;
-			}
-			dbus_message_iter_get_basic(&dict_entry, &property);
-			printf("Property: %s\n", property);
-			if(strcmp(property, name) != 0) continue;
-			if (!dbus_message_iter_next(&dict_entry))
-			{
-				printf("No next!\n");
+      }
+      dbus_message_iter_get_basic(&dict_entry, &property);
+      printf("Property: %s\n", property);
+      if(strcmp(property, name) != 0) continue;
+      if (!dbus_message_iter_next(&dict_entry))
+      {
+        printf("No next!\n");
         return false;
-			}
-			if (dbus_message_iter_get_arg_type(&dict_entry) != DBUS_TYPE_VARIANT)
-			{
-				printf("Iter not a variant!\n");
+      }
+      if (dbus_message_iter_get_arg_type(&dict_entry) != DBUS_TYPE_VARIANT)
+      {
+        printf("Iter not a variant!\n");
         return false;
-			}
-			dbus_message_iter_recurse(&dict_entry, &prop_val);
-			// type = properties[*prop_index].type;
-			if (dbus_message_iter_get_arg_type(&prop_val) != expected_type) {
-      //   LOGE("Property type mismatch in get_property: %d, expected:%d, index:%d",
-      //        dbus_message_dict_entry_get_arg_type(&prop_val), type, *prop_index);
-				printf("Not the type we expect!\n");
-				return false;
-			}
-			dbus_message_iter_get_basic(&prop_val, &val);
-			return true;
+      }
+      dbus_message_iter_recurse(&dict_entry, &prop_val);
+      // type = properties[*prop_index].type;
+      if (dbus_message_iter_get_arg_type(&prop_val) != expected_type) {
+        //   LOGE("Property type mismatch in get_property: %d, expected:%d, index:%d",
+        //        dbus_message_dict_entry_get_arg_type(&prop_val), type, *prop_index);
+        printf("Not the type we expect!\n");
+        return false;
+      }
+      dbus_message_iter_get_basic(&prop_val, &val);
+      return true;
     } while(dbus_message_iter_next(&dict));
-		return false;
+    return false;
   }
 
-	
+  
 protected:
-  DBusConnection* mConnection;	
+  DBusConnection* mConnection;  
 };
 
 RawDBusConnection::RawDBusConnection() {
@@ -189,11 +189,11 @@ done:
 }
 
 DBusMessage * RawDBusConnection::dbus_func_args_timeout(int timeout_ms,
-																												const char *path,
-																												const char *ifc,
-																												const char *func,
-																												int first_arg_type,
-																												...) {
+                                                        const char *path,
+                                                        const char *ifc,
+                                                        const char *func,
+                                                        int first_arg_type,
+                                                        ...) {
   DBusMessage *ret;
   va_list lst;
   va_start(lst, first_arg_type);
@@ -205,11 +205,11 @@ DBusMessage * RawDBusConnection::dbus_func_args_timeout(int timeout_ms,
 }
 
 DBusMessage * RawDBusConnection::dbus_func_args(                           
-	const char *path,
-	const char *ifc,
-	const char *func,
-	int first_arg_type,
-	...) {
+  const char *path,
+  const char *ifc,
+  const char *func,
+  int first_arg_type,
+  ...) {
   DBusMessage *ret;
   va_list lst;
   va_start(lst, first_arg_type);
@@ -221,11 +221,11 @@ DBusMessage * RawDBusConnection::dbus_func_args(
 }
 
 DBusMessage * RawDBusConnection::dbus_func_args_error(DBusError *err,
-																											const char *path,
-																											const char *ifc,
-																											const char *func,
-																											int first_arg_type,
-																											...) {
+                                                      const char *path,
+                                                      const char *ifc,
+                                                      const char *func,
+                                                      int first_arg_type,
+                                                      ...) {
   DBusMessage *ret;
   va_list lst;
   va_start(lst, first_arg_type);
@@ -307,17 +307,17 @@ bool RawDBusConnection::dbus_returns_boolean(DBusMessage *reply) {
 
 class BluetoothAdapter : public RawDBusConnection {
 public:
-	BluetoothAdapter();
-	~BluetoothAdapter() {}
+  BluetoothAdapter();
+  ~BluetoothAdapter() {}
 
-	void get_adapter_path ();
-	void get_adapter_properties();
-	const char* mAdapterPath;
-	bool mPowered;
+  void get_adapter_path ();
+  void get_adapter_properties();
+  const char* mAdapterPath;
+  bool mPowered;
   bool mDiscoverable;
   uint32_t mClass;
-	const char* mAddress;
-	const char* mName;
+  const char* mAddress;
+  const char* mName;
   bool mPairable;
   uint32_t mPairableTimeout;
   uint32_t mDiscoverableTimeout;
@@ -325,101 +325,101 @@ public:
 };
 
 BluetoothAdapter::BluetoothAdapter() {
-	
+  
 }
 
 void BluetoothAdapter::get_adapter_path() {
   DBusMessage *msg = NULL, *reply = NULL;
-	DBusError err;
-	const char *device_path = NULL;
-	int attempt = 0;
+  DBusError err;
+  const char *device_path = NULL;
+  int attempt = 0;
 
-	for (attempt = 0; attempt < 1000 && reply == NULL; attempt ++) {
-		msg = dbus_message_new_method_call("org.bluez", "/",
-																			 "org.bluez.Manager", "DefaultAdapter");
-		if (!msg) {
-			printf("%s: Can't allocate new method call for get_adapter_path!",
-						 __FUNCTION__);
-			return;
-		}
-		dbus_message_append_args(msg, DBUS_TYPE_INVALID);
-		dbus_error_init(&err);
-		reply = dbus_connection_send_with_reply_and_block(mConnection, msg, -1, &err);
+  for (attempt = 0; attempt < 1000 && reply == NULL; attempt ++) {
+    msg = dbus_message_new_method_call("org.bluez", "/",
+                                       "org.bluez.Manager", "DefaultAdapter");
+    if (!msg) {
+      printf("%s: Can't allocate new method call for get_adapter_path!",
+             __FUNCTION__);
+      return;
+    }
+    dbus_message_append_args(msg, DBUS_TYPE_INVALID);
+    dbus_error_init(&err);
+    reply = dbus_connection_send_with_reply_and_block(mConnection, msg, -1, &err);
 
-		if (!reply) {
-			if (dbus_error_is_set(&err)) {
-				if (dbus_error_has_name(&err,
-																"org.freedesktop.DBus.Error.ServiceUnknown")) {
-					// bluetoothd is still down, retry
-					//LOG_AND_FREE_DBUS_ERROR(&err);
-					printf("Service unknown\n");
-					usleep(10000);  // 10 ms
-					continue;
-				} else {
-					// Some other error we weren't expecting
-					printf("other error\n");
-					//LOG_AND_FREE_DBUS_ERROR(&err);
-				}
-			}
-			goto failed;
-		}
-	}
-	if (attempt == 1000) {
-		printf("timeout\n");
-		//LOGE("Time out while trying to get Adapter path, is bluetoothd up ?");
-		goto failed;
-	}
+    if (!reply) {
+      if (dbus_error_is_set(&err)) {
+        if (dbus_error_has_name(&err,
+                                "org.freedesktop.DBus.Error.ServiceUnknown")) {
+          // bluetoothd is still down, retry
+          //LOG_AND_FREE_DBUS_ERROR(&err);
+          printf("Service unknown\n");
+          usleep(10000);  // 10 ms
+          continue;
+        } else {
+          // Some other error we weren't expecting
+          printf("other error\n");
+          //LOG_AND_FREE_DBUS_ERROR(&err);
+        }
+      }
+      goto failed;
+    }
+  }
+  if (attempt == 1000) {
+    printf("timeout\n");
+    //LOGE("Time out while trying to get Adapter path, is bluetoothd up ?");
+    goto failed;
+  }
 
-	if (!dbus_message_get_args(reply, &err, DBUS_TYPE_OBJECT_PATH,
-														 &device_path, DBUS_TYPE_INVALID)
-			|| !device_path){
-		if (dbus_error_is_set(&err)) {
-			//LOG_AND_FREE_DBUS_ERROR(&err);
-		}
-		goto failed;
-	}
-	dbus_message_unref(msg);
-	printf("Adapter path: %s\n", device_path);
-	mAdapterPath = device_path;
-	return;
+  if (!dbus_message_get_args(reply, &err, DBUS_TYPE_OBJECT_PATH,
+                             &device_path, DBUS_TYPE_INVALID)
+      || !device_path){
+    if (dbus_error_is_set(&err)) {
+      //LOG_AND_FREE_DBUS_ERROR(&err);
+    }
+    goto failed;
+  }
+  dbus_message_unref(msg);
+  printf("Adapter path: %s\n", device_path);
+  mAdapterPath = device_path;
+  return;
 failed:
-	dbus_message_unref(msg);
+  dbus_message_unref(msg);
 }
 
 void BluetoothAdapter::get_adapter_properties() {
-	DBusMessage *msg, *reply;
-	DBusError err;
-	dbus_error_init(&err);
+  DBusMessage *msg, *reply;
+  DBusError err;
+  dbus_error_init(&err);
 
-	reply = dbus_func_args(mAdapterPath,
-												 DBUS_ADAPTER_IFACE, "GetProperties",
-												 DBUS_TYPE_INVALID);
-	if (!reply) {
-		if (dbus_error_is_set(&err)) {
-			//LOG_AND_FREE_DBUS_ERROR(&err);
-			printf("Error!\n");
-		} else
-			printf("DBus reply is NULL in function %s\n", __FUNCTION__);
-		return ;
-	}
-	
-	DBusMessageIter iter;
-	//jobjectArray str_array = NULL;
-	if (!dbus_message_iter_init(reply, &iter)) {
-		printf("Return value is wrong!\n");
-		dbus_message_unref(reply);
-		return;
-	}
-	//str_array = parse_adapter_properties(env, &iter);
-	GetDBusDictValue<const char*>(iter, "Name", DBUS_TYPE_STRING, mName);
-	printf("name! %s\n", mName);
-	dbus_message_unref(reply);
-	printf("Adapter properties got\n");
+  reply = dbus_func_args(mAdapterPath,
+                         DBUS_ADAPTER_IFACE, "GetProperties",
+                         DBUS_TYPE_INVALID);
+  if (!reply) {
+    if (dbus_error_is_set(&err)) {
+      //LOG_AND_FREE_DBUS_ERROR(&err);
+      printf("Error!\n");
+    } else
+      printf("DBus reply is NULL in function %s\n", __FUNCTION__);
+    return ;
+  }
+  
+  DBusMessageIter iter;
+  //jobjectArray str_array = NULL;
+  if (!dbus_message_iter_init(reply, &iter)) {
+    printf("Return value is wrong!\n");
+    dbus_message_unref(reply);
+    return;
+  }
+  //str_array = parse_adapter_properties(env, &iter);
+  GetDBusDictValue<const char*>(iter, "Name", DBUS_TYPE_STRING, mName);
+  printf("name! %s\n", mName);
+  dbus_message_unref(reply);
+  printf("Adapter properties got\n");
 }
 
 int main(int argc, char** argv) {
-	BluetoothAdapter b;
-	b.createDBusConnection();
-	b.get_adapter_path();
-	b.get_adapter_properties();
+  BluetoothAdapter b;
+  b.createDBusConnection();
+  b.get_adapter_path();
+  b.get_adapter_properties();
 }
